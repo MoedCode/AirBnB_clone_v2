9f9fab505f777102bc848cd5ea88d69581ce36cd
MoedCode <mohamed71291@gmail.com>
2023-12-19 02:27:03 +0200

    state.pystate.py class State (Base): (automated command)

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""contains the class definition of a State and an instance """
+
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State (Base):
+    """state class proto = Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)

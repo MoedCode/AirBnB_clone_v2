002bf9de771251e5b5f4e6efa1f63257ea0bcf47
MoedCode <mohamed71291@gmail.com>
2023-12-19 02:25:09 +0200

    city.py class City(Base): (automated command)

--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""Definition of the State class"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(Base):
+    """class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

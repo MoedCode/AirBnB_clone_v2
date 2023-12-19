f1b37e2518d1db30597019801a9b193f6f6ed21a
MoedCode <mohamed71291@gmail.com>
2023-12-19 18:25:59 +0200

    task 5 Delete object (automated command)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,27 +9,50 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """
+        Retrieve the dictionary's stored in objects or filter by class.
+        methods parameters
+        cls (optional): If specified, filter objects by the given class.
+        Returns:
+        dict: dictionary contains objects stored in __objects or filtered by class.
+        """
         if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
+            filt_dicts = {}
+            for object_key, object_instance in self.__objects.items():
+                if cls == object_instance.__class__ or cls == object_instance.__class__.__name__:
+                    filt_dicts[object_key] = object_instance
+            return filt_dicts
         return self.__objects
+
+    # def new(self, obj):
+    #     """Adds new object to storage dictionary"""
+    #     self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        if obj is not None:
+            new_obj_name = obj.__class__.__name__ + "." + obj.id
+            self.__objects[new_obj_name] = obj
 
+    # def save(self):
+    #     """Saves storage dictionary to file"""
+    #     with open(FileStorage.__file_path, 'w') as f:
+    #         temp = {}
+    #         temp.update(FileStorage.__objects)
+    #         for key, val in temp.items():
+    #             temp[key] = val.to_dict()
+    #         json.dump(temp, f)
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """Serializes and writes objects to the JSON file."""
+        serialized_objects = {}
+
+        # Serialize each object in __objects
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        # Write serialized objects to the JSON file
+        with open(self.__file_path, 'w') as FILE:
+            json.dump(serialized_objects, FILE)
 
     def reload(self):
         """Loads storage dictionary from file"""

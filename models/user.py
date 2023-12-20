787f299374281e861c13ae0afc5e685021ae085c
MoedCode <mohamed71291@gmail.com>
2023-12-20 17:19:56 +0200

    7. DBStorage - User   (automated command)

--- models/user.py
+++ models/user.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" City Module for HBNB project """
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.base_model import BaseModel, Base
 from models.base_model import BaseModel
+from models.city import City
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
+
+# class User(BaseModel):
+#     """This class defines a user by various attributes"""
+#     email = ''
+#     password = ''
+#     first_name = ''
+#     last_name = ''

56796bf897ce78038bc7c833aac498a31476cbb8
MoedCode <mohamed71291@gmail.com>
2023-12-21 04:54:30 +0200

    Review (automated command)

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage DB Storage for hbnb clone"""
+"""Defines a class for managing DB Storage for hbnb clone"""
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.state import State
@@ -18,16 +18,18 @@ classes = {"City": City, "State": State, "Place": Place, "Review": Review,
 
 
 class DBStorage:
+    """Class for managing DB Storage for hbnb clone"""
     __engine = None
     __session = None
 
     def __init__(self):
+        """Initializes the DBStorage instance"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'. format(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
                 HBNB_MYSQL_USER,
                 HBNB_MYSQL_PWD,
                 HBNB_MYSQL_HOST,
@@ -36,6 +38,28 @@ class DBStorage:
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
+    def reload(self):
+        """Reloads the database"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def new(self, obj):
+        """Adds a new object to storage dictionary"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes obj from the current database session"""
+        if obj is None:
+            return
+        self.__session.delete(obj)
+
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         my_dict = {}
@@ -43,34 +67,12 @@ class DBStorage:
             for obj in classes:
                 table = self.__session.query(classes[obj]).all()
                 for obj in table:
-                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
                     my_dict[key] = obj
             return my_dict
         else:
             table = self.__session.query(cls).all()
         for obj in table:
-            key = f"{obj.__class__.__name__}.{obj.id}"
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             my_dict[key] = obj
         return my_dict
-
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.__session.add(obj)
-
-    def save(self):
-        """commit all changes of the current database session"""
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """delete from the current database session"""
-        if obj is None:
-            return
-        self.__session.delete(obj)
-
-    def reload(self):
-        """reload method"""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()

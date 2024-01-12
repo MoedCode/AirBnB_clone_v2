55ae3926f2c726f9f3a8bccbe8eac783a8228639
MoedCode <mohamed71291@gmail.com>
2024-01-12 02:32:44 +0200

    1. Compress before sending (automated command)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+Fabric script (1-pack_web_static.py) for generating a compressed archive
+from the contents of the web_static folder in an AirBnB Clone repository.
+The archive is stored in the 'versions' folder, following the naming convention
+web_static_<year><month><day><hour><minute><second>.tgz.
+"""
 from fabric.api import local
 from time import strftime
 from datetime import date
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
-
-    filename = strftime("%Y%m%d%H%M%S")
+    """Generates a .tgz archive of the web_static folder, stores it in 'versions',
+    and returns the archive path. Returns None in case of any errors.
+    """
+    FILE = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+              .format(FILE))
 
-        return "versions/web_static_{}.tgz".format(filename)
+        return "versions/web_static_{}.tgz".format(FILE)
 
     except Exception as e:
         return None

5f20156f884e042d69d91a7c346c287a1c20a8b9
MoedCode <mohamed71291@gmail.com>
2024-01-12 02:28:23 +0200

    1. Compress before sending (automated command)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
 from fabric.api import local
 from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """
-    Generates a compressed archive of the contents in the web_static folder.
+    """ A script that generates archive the contents of web_static folder"""
 
-    Returns:
-        str: The path to the created archive, or None if an error occurs.
-    """
-
-    # Generate a timestamp to make the archive file unique
     filename = strftime("%Y%m%d%H%M%S")
-
     try:
-        # Create the 'versions' directory if it doesn't exist
         local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
-        # Use tar to create a compressed archive of the web_static folder
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(filename))
-
-        # Return the path to the created archive
         return "versions/web_static_{}.tgz".format(filename)
 
     except Exception as e:
-        # If an error occurs during the process, return None
         return None
-
-# Example usage:
-# archive_path = do_pack()
-# if archive_path:
-#     print(f"Archive created: {archive_path}")
-# else:
-#     print("Error creating archive.")

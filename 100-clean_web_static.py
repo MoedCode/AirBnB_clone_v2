093ec65832ac3c92a86ae7af060fab5760a6c93a
MoedCode <mohamed71291@gmail.com>
2024-01-12 03:08:28 +0200

    4. Keep it clean! (automated command)

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+import os
+from fabric.api import *
+
+env.hosts = ['100.25.19.204', '54.157.159.85']
+
+
 def do_clean(number=0):
-    """Deletes out-of-date archives."""
-    if number < 0:
-        return
-    number = 1 if number == 0 else number
+    """Delete out-of-date archives.
 
-    with cd('/data/web_static/releases'):
-        # List all archives in versions folder
-        archives = run("ls -lt versions").split()
-        archives = [
-            archive for archive in archives if archive.startswith('web_static_')]
+    Args:
+        number (int): The number of archives to keep.
 
-        # Sort archives by modification time in reverse order
-        archives.sort(key=lambda x: datetime.strptime(
-            x.split('.')[0], 'web_static_%Y%m%d%H%M%S'), reverse=True)
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-        # Keep only the required number of archives
-        archives_to_keep = archives[:number]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-        # Delete unnecessary archives
-        for archive in archives:
-            if archive not in archives_to_keep:
-                run("rm -f versions/{}".format(archive))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]

e0dd3afb24214071555d1ba4dc520d260cc45bbe
MoedCode <mohamed71291@gmail.com>
2024-01-28 01:03:18 +0200

    7. Improve engines db_storage.py (automated command)

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class for managing DB Storage for the HBNB clone"""
-
+"""Defines a class for managing DB Storage for hbnb clone"""
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.state import State
@@ -14,11 +13,12 @@ from os import getenv
 import sqlalchemy as db
 
 classes = {"City": City, "State": State, "Place": Place, "Review": Review,
-           "Amenity": User, "User": Amenity}
+           "Amenity": User, "User": Amenity
+           }
 
 
 class DBStorage:
-    """Class for managing DB Storage for the HBNB clone"""
+    """Class for managing DB Storage for hbnb clone"""
     __engine = None
     __session = None
 
@@ -29,7 +29,11 @@ class DBStorage:
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         self.__engine = create_engine(
-            f'mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}',
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                HBNB_MYSQL_USER,
+                HBNB_MYSQL_PWD,
+                HBNB_MYSQL_HOST,
+                HBNB_MYSQL_DB),
             pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
@@ -43,34 +47,36 @@ class DBStorage:
         self.__session = Session()
 
     def new(self, obj):
-        """Adds a new object to storage"""
+        """Adds a new object to storage dictionary"""
         self.__session.add(obj)
 
     def save(self):
-        """Commits all changes of the current session"""
+        """Commits all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Deletes obj from the current session"""
-        if obj:
-            self.__session.delete(obj)
+        """Deletes obj from the current database session"""
+        if obj is None:
+            return
+        self.__session.delete(obj)
 
     def all(self, cls=None):
-        """Returns a dictionary of models in storage"""
-        model_dict = {}
+        """Returns a dictionary of models currently in storage"""
+        my_dict = {}
         if cls is None:
-            for obj_type in classes:
-                objs = self.__session.query(classes[obj_type]).all()
-                for obj in objs:
-                    key = f"{obj.__class__.__name__}.{obj.id}"
-                    model_dict[key] = obj
+            for obj in classes:
+                table = self.__session.query(classes[obj]).all()
+                for obj in table:
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    my_dict[key] = obj
+            return my_dict
         else:
-            objs = self.__session.query(cls).all()
-            for obj in objs:
-                key = f"{obj.__class__.__name__}.{obj.id}"
-                model_dict[key] = obj
-        return model_dict
+            table = self.__session.query(cls).all()
+        for obj in table:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            my_dict[key] = obj
+        return my_dict
 
     def close(self):
         """Closes the session"""
-        self.__session.close()
+        self.__session.remove()

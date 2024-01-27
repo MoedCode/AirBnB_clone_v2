3908b793ce1b73d09b71c9c7e1e940ef88155673
MoedCode <mohamed71291@gmail.com>
2024-01-27 23:06:20 +0200

    7. Improve engines (automated command)

--- models/state.py
+++ models/state.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" State Module for HBNB project """
 from os import getenv
-from sqlalchemy import String, Column, ForeignKey
-from sqlalchemy.orm import relationship
-import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-
-from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-
+    __tablename__ = "states"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="delete")
 
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    elif getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            cit_vals_list = []
-            Cits_dict = models.storage.all('City')
-            for city, value in Cits_dict.items():
-                if self.id == city.state_id:
-                    cit_vals_list.append(value)
-            return cit_vals_list
-#     """ State class """
-#     name = ""
+            cities_list = list(models.storage.all(City).values())
+            return [city for city in cities_list if city.state_id == self.id]

5d2258d76edbc013aac0188573d30ac6b5b42b44
MoedCode <mohamed71291@gmail.com>
2024-01-27 02:10:00 +0200

    0. Hello Flask! (automated command)

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"starts a Flask web application"
+from flask import Flask
+
+APP = Flask(__name__)
+
+APP.url_map.strict_slashes = False
+@APP.route("/")
+def homepage():
+    "Routing The Home Page"
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=5000)

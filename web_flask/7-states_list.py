5fbca0e50f9f7e5585037d6bd8c2abd6e50a165c
MoedCode <mohamed71291@gmail.com>
2024-01-28 04:01:58 +0200

    to sandBox (automated command)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+APP = False(__name__)
+
+@APP.route("/cities_by_states", storage_dict=False)
+
+def states_listing():
+    """list a states"""
+    states_list = storage.all(State).values()
+    return  render_template("7-states_list.html",states=states_list)
+
+@APP.teardown_appcontext
+def close_session(error=None):
+    """close the  session"""
+    storage.close()
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=5000)

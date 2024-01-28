1f10498aa7efe5a426229497950f9fdd002b3d3f
MoedCode <mohamed71291@gmail.com>
2024-01-28 18:22:22 +0200

    8. List of states (automated command)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from models.city import City
+
 app = Flask(__name__)
 
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
+@app.route("/cities_by_states", strict_slashes=False)
+def states_listing():
+    """List states"""
+    states_list = storage.all(State)
+    return render_template("7-states_list.html", states=states_list)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_session(error=None):
+    """Close the session"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

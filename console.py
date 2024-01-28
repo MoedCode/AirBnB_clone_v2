e8931e59863013146590b8d8cb77c66357d046b6
MoedCode <mohamed71291@gmail.com>
2024-01-28 04:53:29 +0200

    END TST (automated command)

--- console.py
+++ console.py
@@ -2,16 +2,16 @@
 """ HBNB Console Module """
 
 import cmd
-from datetime import datetime
-import models
-from models.amenity import Amenity
+import sys
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from models.__init__ import storage
 from models.user import User
-import shlex  # Used for splitting the line along spaces, except in double quotes
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import shlex
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 """  instantiates  object of FileStorage class """
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE") == 'db':
+if getenv("HBNB_TYPE_STORAGE") != 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:

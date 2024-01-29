a482a2aff2050fd8602e4387579264f97752da31
MoedCode <mohamed71291@gmail.com>
2024-01-29 04:00:05 +0200

    10. States and State (automated command)

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""0. Hello Flask!"""
+from flask import Flask, render_template
+from models.state import State
+from models.amenity import Amenity
+from models import storage
+
+
+
+APP = Flask(__name__)
+APP.url_map.strict_slashes = False
+
+@APP.route("/hbnb_filters")
+def lists_states_cls():
+    data = {
+    'states' : storage.all(State).values(),
+    'amenities': storage.all(Amenity).values()
+    }
+    return render_template("10-hbnb_filters.html", models=data)
+
+
+@APP.teardown_appcontext
+def close_db(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-"""starts a Flask web application:"""
+"""0. Hello Flask!"""
 from flask import Flask, render_template
-from models import storage
 from models.state import State
-from models.city import City
+from models import storage
+
 
 APP = Flask(__name__)
+APP.url_map.strict_slashes = False
 
-
-@APP.route("/states", strict_slashes=False)
-def list_states():
-    """print a number"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, states_only=True)
-
-
-@APP.route("/states/<id>", strict_slashes=False)
-def list_city_state(id):
-    """display the state and it's cities"""
-    result = []
-    state_name = ''
+@APP.route("/states")
+def listing_sates_id():
     states = storage.all(State).values()
-    cities = storage.all(City).values()
-    for state in states:
-        if state.id == id:
-            state_name = state.name
-    for city in cities:
-        if id == city.state_id:
-            result.append(city)
-    return render_template('9-states.html',
-                           result=result, states_only=False,
-                           name=state_name)
+    return render_template("7-states_list.html", states=states)
+
+
+@APP.route("/states/<id>")
+def state_id(id):
+    state = None
+    for each_St in storage.all(State).values():
+        if each_St.id == id:
+            state = each_St
+            break
+    return render_template("9-states.html", state=state)
 
 
 @APP.teardown_appcontext
-def cleanDb(error=None):
-    """close the session"""
+def close_db_sess(exception=None):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    APP.run(host="0.0.0.0", port=5000)
